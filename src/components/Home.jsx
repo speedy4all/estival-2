891507c5b9431491e9d6c06d6104094173f7c69e
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-26 12:27:44 +0300

    cc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = props => {
     <React.Fragment>
       <h1>My site</h1>
       <div>
-        <p>This is Ball          Story           site application</p>
+        <p>This is Ball          Story   hello        site application</p>
         <button onClick={() => {
             props.switchPage("About");
         }} >About page</button>

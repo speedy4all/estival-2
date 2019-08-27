c2d2e35088bf7c1138e5b0bebf8cf6bae5f588fe
Maria Stefan <maya.maria_2001@yahoo.com>
2019-08-27 12:03:48 +0300

    mmm

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = props => {
   return (
     <React.Fragment>
-      <h1>My site</h1>
+      <h1>tripME</h1>
       <div>
         <p>This is TRIP_ME application</p>
         <button onClick={()  => {

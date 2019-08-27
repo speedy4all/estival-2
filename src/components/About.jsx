287daac801e101ba4d34b995874ad4cd865a9456
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-27 13:30:22 +0300

    ceva

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { Button } from "reactstrap"
+import { Button,Row } from "reactstrap";
+
 const About = props => {
   return (
     <React.Fragment>
-      
       <h1>Nothing for now</h1>
       <div>
-        
-        <button onClick={props.switchPage}><Button color="danger">Back home</Button>{' '}</button>
+        <button onClick={props.switchPage}>
+          <Button color="danger">Back home</Button>{" "}
+        </button>
       </div>
-      <div class="row">
+      {/*<div class="row">
   <div class="col-sm-4"></div>
   <div class="col-sm-8"><Button color="primary" size="lg" block>Alegerea 1</Button></div>
 </div>
 <div class="row">
   <div class="col-sm-4"></div>
   <div class="col-sm-8"><Button color="primary" size="lg" block>Alegerea 2</Button></div>
-</div>
-      
-      
+  </div>*/}
+
+      <div className="d-flex flex-column align-items-end mr-3 justify-content-end" style={{height:'100vh'}}>
+        
+
+        <Row><Button color="primary" size="lg" block>Alegerea 1</Button></Row>
+
+        <Row><Button color="primary" size="lg" block>Alegerea 2</Button></Row>
+        
+      </div>
     </React.Fragment>
   );
 };
-
-
 
 export default About;

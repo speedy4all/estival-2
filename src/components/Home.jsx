682f29cab0a2e33c11266009f06a211f865ba53c
Ivascu Andrei <ivascuai2@yahoo.com>
2019-08-27 10:40:21 +0300

    image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = props => {
   return (
     <React.Fragment>
       
-      <div>
+      <div class="div1">
         <h1>Ball story</h1>
         <p>Ball journey</p>
         <p>This is Ball Story site application</p>

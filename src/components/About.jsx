72911e02b470846071b1ca19615a0841573cfbeb
Ivascu Andrei <ivascuai2@yahoo.com>
2019-08-27 13:32:02 +0300

    fara butoane

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,22 +3,18 @@ import { Button } from "reactstrap"
 const About = props => {
   return (
     <React.Fragment>
-      
-      <h1>Nothing for now</h1>
+
       <div>
-        
+
         <button onClick={props.switchPage}><Button color="danger">Back home</Button>{' '}</button>
       </div>
-      <div class="row">
-  <div class="col-sm-4"></div>
-  <div class="col-sm-8"><Button color="primary" size="lg" block>Alegerea 1</Button></div>
-</div>
-<div class="row">
-  <div class="col-sm-4"></div>
-  <div class="col-sm-8"><Button color="primary" size="lg" block>Alegerea 2</Button></div>
-</div>
       
-      
+
+
+
+
+
+
     </React.Fragment>
   );
 };

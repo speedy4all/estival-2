5d6f007d6beb19cfaf48a3f9b55ac17cd65d0e85
Andrei Saramet <andreisaramet01@yahoo.com>
2019-08-27 12:38:06 +0300

    Multe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap'
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import { Button } from 'reactstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Home = props => {
   return (
     <React.Fragment>
-      <h1>My site</h1>
-      <div>
-        <InputGroup >
-          <InputGroupAddon addonType="prepend">search</InputGroupAddon>
-          <Input placeholder="username" />
-          <InputGroupAddon addonType="prepend">enter</InputGroupAddon>
-        </InputGroup>
-        <p>This is TRIP_ME application</p>
+      <Container>
+        <Row>
+          <Col>tripME</Col>
+          <Col><InputGroup >
+            <InputGroupAddon addonType="prepend">search</InputGroupAddon>
+            <Input placeholder="username" />
+            <Button color="info" onClick={() => {
+              props.switchPage("About");
+            }} >enter</Button>{' '}
+          </InputGroup>
+          </Col>
 
-        <button onClick={() => {
-          props.switchPage("About");
-        }} >About page</button>
-
-
-      </div>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 };

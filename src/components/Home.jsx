59b5c2d2b0d9adcdf1df41c32ff2d35cd534489e
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-27 11:47:11 +0300

    schimbari

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,10 @@ const Home = props => {
     <React.Fragment>
       
       <div class="div1">
-        <h1>Ball story</h1>
-        <p>Ball journey</p>
-        <p>This is Ball Story site application</p>
-        <button   onClick={() => {
+        <h1 class="titlu">Ball story</h1>
+        <button class="buttonStart"   onClick={() => {
             props.switchPage("About");
-        }} ><Button color="success">Start</Button>{' '}</button>
+        }} ><Button color="success">Start Game</Button>{' '}</button>
        
       </div>
     </React.Fragment>

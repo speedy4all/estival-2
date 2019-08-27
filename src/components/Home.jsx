210e492bea805696ab1584ea6380a5fc674020c0
Codrut Plaiasu <codrut.plaiasu35@gmail.com>
2019-08-27 12:21:42 +0300

    ss

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import './style.css';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap'
 const Home = props => {
   return (
     <React.Fragment>
-      <h1>My site</h1>
+      <h1 className = "titlu1">Trip Me</h1>
       <div>
         <InputGroup >
+          <div className = "titlu">
           <InputGroupAddon addonType="prepend">search</InputGroupAddon>
           <Input placeholder="username" />
+          </div>
         </InputGroup>
         <p>This is TRIP_ME application</p>
 

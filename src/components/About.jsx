6bf888392747f6deb387513a403beb508adaace2
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-27 14:14:34 +0300

    ceva

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,27 +1,34 @@
 import React from "react";
-import { Button,Row } from "reactstrap";
+import { Button, Row } from "reactstrap";
+import "../Stile.css";
 
 const About = props => {
   return (
     <React.Fragment>
+      <div class="bg">
+        <div>
+          <button onClick={props.switchPage}>
+            <Button color="danger">Back home</Button>{" "}
+          </button>
+        </div>
 
+        <div
+          className="d-flex flex-column align-items-end mr-3 justify-content-end"
+          style={{ height: "87vh" }}
+        >
+          <Row>
+            <Button color="primary" size="lg" block>
+              Alegerea 1
+            </Button>
+          </Row>
 
-      <div>
-
-        <button onClick={props.switchPage}><Button color="danger">Back home</Button>{' '}</button>
+          <Row>
+            <Button color="primary" size="lg" block>
+              Alegerea 2
+            </Button>
+          </Row>
+        </div>
       </div>
-     
-      
-
-      <div className="d-flex flex-column align-items-end mr-3 justify-content-end" style={{height:'87vh'}}>
-        
-
-        <Row><Button color="primary" size="lg" block>Alegerea 1</Button></Row>
-
-        <Row><Button color="primary" size="lg" block>Alegerea 2</Button></Row>
-        
-      </div>
-
     </React.Fragment>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import "../Stile.css"
-import { Button } from 'reactstrap';
+import "../Stile.css";
+import { Button } from "reactstrap";
 
 const Home = props => {
   return (
     <React.Fragment>
-      
-      
       <div class="div1">
-        
         <h1 class="titlu">Ball story</h1>
-        <button class="buttonStart"   onClick={() => {
-          props.switchPage("About");
-        }} ><Button color="success">Start Game</Button>{' '}</button>
-       
-        </div>
-      </React.Fragment>
+        <button
+          class="buttonStart"
+          onClick={() => {
+            props.switchPage("About");
+          }}
+        >
+          <Button color="success">Start Game</Button>{" "}
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
-  export default Home;
+export default Home;

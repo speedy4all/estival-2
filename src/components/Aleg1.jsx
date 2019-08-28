51643bd44dc5235e81bc461dd2b691a252b79c05
Ivascu Andrei <ivascuai2@yahoo.com>
2019-08-28 14:18:39 +0300

    missing import

--- src/components/Aleg1.jsx
+++ src/components/Aleg1.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import "../Stile.css";
+import { Button } from 'reactstrap';
 const Aleg1 = props => {
     return (
-         
-         <div class="bg1" style={{ height: "87vh" }}>
-          <button  onClick={() => props.switchToPage('aleg2')}><Button color="primary" size="sm">Alegere 3</Button>{' '}</button>
-          <button onClick={() => props.switchToPage('aleg3')}>Alege 4</button>
-        
-         </div>
-        
+
+        <div class="bg1" style={{ height: "87vh" }}>
+            <button onClick={() => props.switchToPage('aleg2')}><Button color="primary" size="sm">Alegere 3</Button>{' '}</button>
+            <button onClick={() => props.switchToPage('aleg3')}>Alege 4</button>
+
+        </div>
+
     );
 };
 

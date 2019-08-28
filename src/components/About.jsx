27cc3566006fb44ffdff66f8f20a230bc30186e7
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-28 11:44:00 +0300

    about finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,8 +3,6 @@ import { Button, Row, Alert } from "reactstrap";
 import "../Stile.css";
 import Typist from "react-typist";
 
-
-
 const About = props => {
   return (
     <React.Fragment>
@@ -24,17 +22,6 @@ const About = props => {
             src="https://www.shitpostbot.com/resize/585/400?img=%2Fimg%2Fsourceimages%2Fcalypso-twisted-metal-5a86b4cd8e306.jpeg"
           ></img>
 
-          <div>
-            <Alert color="primary">
-              <Typist>
-                Salut Minge, te pot ajuta eu sa scapi de aici, urmeaza-ma.
-            </Typist>
-            </Alert>
-
-          </div>
-
-
-
           <Row>
             <Button
               color="primary"
@@ -52,22 +39,16 @@ const About = props => {
             </Button>
           </Row>
         </div>
-        <div className="d-flex align-items-end justify-content-center">
+      </div>
+      <div class="text" className="d-flex align-items-end justify-content-center">
+        <Alert color="primary">
           <Typist>
             Salut Minge, te pot ajuta eu sa scapi de aici, urmeaza-ma.
           </Typist>
-        </div>
+        </Alert>
       </div>
-
-
-
     </React.Fragment>
   );
 };
-
-
-/*{https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7hrkNlZqVxvvASOo85DFkreu5A7ro8Bv_KHJMB9EYu1VOo97m
-https://pbs.twimg.com/media/CqH2o0lW8AAw6q9.jpg}*/
-
 
 export default About;

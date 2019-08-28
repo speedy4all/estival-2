753ccde198ff293e391428a79e49d6448f13b854
Andrei Saramet <andreisaramet01@yahoo.com>
2019-08-28 13:32:31 +0300

    com

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,20 +12,7 @@ const Home = props => {
   return (
     <React.Fragment>
       <div className="Homecss">
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">tripME</NavbarBrand>
-          <InputGroup >
-            <InputGroupAddon addonType="prepend">search</InputGroupAddon>
-            <Input placeholder="destinatie" />
-            <Button color="info" onClick={() => {
-              props.switchPage("About");
-            }} >enter</Button>
-          </InputGroup>
-          <Button className="button1" onClick={() => {
-            props.switchPage("About");
-          }} >Help!
-            </Button>{' '}
-        </Navbar>
+        
 
         <Col sm={2} className="fil">Filters
         <Filter filterData={()=>({})}/>

a4f9e4631ffbe57aaedaf60c6e204e6eec0cbead
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-28 14:46:07 +0300

    aleg1

--- src/components/Aleg1.jsx
+++ src/components/Aleg1.jsx
@@ -1,17 +1,63 @@
 import React from 'react';
+import { Button, Row, Alert } from "reactstrap";
 import "../Stile.css";
-import { Button, Row } from 'reactstrap';
+import Typist from "react-typist";
 
 const Aleg1 = props => {
     return (
 
-        <div class="bg1" style={{ height: "87vh" }}>
-        <Row>
-            <Button color="primary" size="sm" onClick={() => props.switchToPage('aleg2')}>Alegere 3 {' '}</Button>
-        </Row>
-        <Row>
-            <Button color="primary" size="sm" onClick={() => props.switchToPage('aleg3')}>Alegere 4 {' '}</Button>
-        </Row>
+        <div>
+        <div class="bg1" style={{ height: "87vh" } }>
+        <div>
+                <button class="buttonHome" onClick={props.switchPage}>
+                    <Button color="danger">Back home</Button>{" "}
+                </button>
+            </div>
+
+            <div
+                className="d-flex flex-column align-items-end mr-3 "
+                style={{ height: "87vh" }}
+            >
+                <img
+                    class="imagine"
+                    src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/124ca078-9c51-4c1d-9873-11275781abe8/ddbu3ez-48d7e5ed-056e-4fb0-8527-35dd328f602a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEyNGNhMDc4LTljNTEtNGMxZC05ODczLTExMjc1NzgxYWJlOFwvZGRidTNlei00OGQ3ZTVlZC0wNTZlLTRmYjAtODUyNy0zNWRkMzI4ZjYwMmEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TaBKElK-E49GPvcvG3dPNh3Hmeb5c5hwNVEyWhC6X68"
+                ></img>
+
+
+
+
+                <Row>
+                    <Button
+                        color="primary"
+                        size="lg"
+                        block
+                        onClick={() => props.switchToPage("aleg3")}
+                    >
+                        I should go with Man in Black
+      </Button>
+                </Row>
+
+                <Row>
+                    <Button color="primary" size="lg" block onClick={() => props.switchToPage('aleg4')}>
+                        I should go alone
+      </Button>
+                </Row>
+            </div>
+
+        </div>
+        <div className="d-flex align-items-end justify-content-center">
+            <Alert color="primary">
+                <Typist>
+                (Man in Black) : I can help you go home
+    </Typist>
+            </Alert>
+        
+
+        
+        
+            
+        
+        </div>
         </div>
         
 

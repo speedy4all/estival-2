615031e9ab5a38a73140a68d8b835bf92d9269ab
Andrei Saramet <andreisaramet01@yahoo.com>
2019-08-28 11:56:42 +0300

    com

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,56 +12,57 @@ const Home = props => {
   return (
     <React.Fragment>
       <div className="Homecss">
-        <Container>
-          <Row>
-            <Col sm={1}>tripME</Col>
-            <Col sm={10}><InputGroup >
-              <InputGroupAddon addonType="prepend">search</InputGroupAddon>
-              <Input placeholder="destination" />
-              <Button color="info" onClick={() => {
-                props.switchPage("About");
-              }} >enter</Button>{' '}
-            </InputGroup>
-            </Col>
-            <Col sm={1}>
-              <Button className="button1" onClick={() => {
-                props.switchPage("About");
-              }} >Help!</Button>{' '}
-            </Col>
 
-          </Row>
-          <Row >
-            <Col sm={2} className="fil">Filters</Col>
+        <Row>
+          <Col sm={1}>tripME</Col>
+          <Col sm={10}><InputGroup >
+            <InputGroupAddon addonType="prepend">search</InputGroupAddon>
+            <Input placeholder="destination" />
+            <Button color="info" onClick={() => {
+              props.switchPage("About");
+            }} >enter</Button>{' '}
+          </InputGroup>
+          </Col>
+          <Col sm={1}>
+            <Button className="button1" onClick={() => {
+              props.switchPage("About");
+            }} >Help!</Button>{' '}
+          </Col>
 
-          </Row>
-          <Row>
-            <Col className="fil" sm={2}>
-              <Form>
-                <FormGroup>
-                  <Label for="exampleSelect">Clasificare dupa stele</Label>
-                  <Input type="select" name="select" id="exampleSelect">
-                    <option>*</option>
-                    <option>**</option>
-                    <option>***</option>
-                    <option>****</option>
-                    <option>*****</option>
-                  </Input>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="exampleSelect">Distanta fata de centru</Label>
-                  <Input type="select" name="select" id="exampleSelect">
-                    <option>500m</option>
-                    <option>1km</option>
-                    <option>2-5km</option>
-                    <option>5-10km</option>
+        </Row>
 
-                  </Input>
-                </FormGroup>
-              </Form>
-            </Col>
-            <Col sm={10} className="ab"></Col>
-          </Row>
-        </Container>
+        <Row >
+          <Col sm={2} className="fil">Filters</Col>
+
+        </Row>
+        <Row>
+          <Col className="fil" sm={2}>
+            <Form>
+              <FormGroup>
+                <Label for="exampleSelect">Clasificare dupa stele</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>*</option>
+                  <option>**</option>
+                  <option>***</option>
+                  <option>****</option>
+                  <option>*****</option>
+                </Input>
+              </FormGroup>
+              <FormGroup>
+                <Label for="exampleSelect">Distanta fata de centru</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>500m</option>
+                  <option>1km</option>
+                  <option>2-5km</option>
+                  <option>5-10km</option>
+
+                </Input>
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col sm={10} className="ab"></Col>
+        </Row>
+
       </div>
     </React.Fragment>
   );

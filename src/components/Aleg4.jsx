dcaf3e1727706725fb11bb0b9d85a5c0567ce106
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-29 11:55:53 +0300

    ceva

--- src/components/Aleg4.jsx
+++ src/components/Aleg4.jsx
@@ -7,17 +7,14 @@ const Aleg4 = props => {
     return (
 
         <div>
-        <div class="bg1" style={{ height: "87vh" } }>
+        <div class="bg2" style={{ height: "87vh" } }>
         
 
             <div
                 className="d-flex flex-column align-items-end mr-3 "
                 style={{ height: "87vh" }}
             >
-                <img
-                    class="imagine"
-                    src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/124ca078-9c51-4c1d-9873-11275781abe8/ddbu3ez-48d7e5ed-056e-4fb0-8527-35dd328f602a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEyNGNhMDc4LTljNTEtNGMxZC05ODczLTExMjc1NzgxYWJlOFwvZGRidTNlei00OGQ3ZTVlZC0wNTZlLTRmYjAtODUyNy0zNWRkMzI4ZjYwMmEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TaBKElK-E49GPvcvG3dPNh3Hmeb5c5hwNVEyWhC6X68"
-                ></img>
+                
 
 
 

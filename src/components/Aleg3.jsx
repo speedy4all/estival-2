a725b8c2db1f1eb21b24c50442a0e5b41ed58fbf
Grigore Daniel-Florin <grigore_daniel_florin@yahoo.com>
2019-08-29 11:45:14 +0300

    dggd

--- src/components/Aleg3.jsx
+++ src/components/Aleg3.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { Button, Row, Alert } from "reactstrap";
 const Aleg3 = props => {
     return (
      
-      <div class="div1">
-            <center>GAME OVER</center>
-            <button onClick={() => props.switchToPage('Home')}>Restart</button>
+      <div class="div2" style={{ height: "100vh" }}>
+            <center class="gameover">GAME OVER</center>
+            <div>
+                <button class="buttonHome" onClick={() => props.switchToPage('Home')}>
+                    <Button color="danger">Restart</Button>{" "}
+                </button>
+                <img class="goimage" src="https://www196.lunapic.com/editor/working/15670669902556798?9435129990"></img>
+            </div>
         </div>
         
     );

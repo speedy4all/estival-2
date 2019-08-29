9099f7cb0e8651667b1916acdd8a3df66b0dd8ae
Robert Bita <bitza.roby@yahoo.com>
2019-08-29 14:01:19 +0300

    asdg

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -5,18 +5,18 @@ import Typist from "react-typist";
 
 const Story = props => (
 
-    <div>
-        <Row>
-          <Button
-            color="primary"
-            size="lg"
-            block
-            onClick={() => props.switchToPage("FirstPage")}
-          >
-            I should go with the Man in Black
-          </Button>
-        </Row>
+    <div class="divStory">
+        <Button
+              color="primary"
+              size="lg"
+              block
+              onClick={() => props.switchToPage("FirstPage")}
+            >
+              Open the door.
+            </Button>
+       
     </div>
+    
     
 
 );
